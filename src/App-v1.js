23c2129f155c759d61ec6a87c70cea9f07635885
Robert <pinneyr@hotmail.com>
2023-10-17 18:24:55 +0200

    add children props

--- src/App-v1.js
+++ src/App-v1.js
@@ -41,42 +41,26 @@ function Steps() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
           <div>
-            <Stepmessage step={step}>{messages[step - 1]}</Stepmessage>
+            <p className="message">
+              Step {step}: {messages[step - 1]}
+            </p>
             <div className="buttons">
-              <Button
-                bgColor="#7950f2"
-                textColor="#fff"
+              <button
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
                 onClick={handlePrevious}
               >
-                <span>👈</span>Previous
-              </Button>
-              <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-                Next<span>👉</span>
-              </Button>
+                Previous
+              </button>
+              <button
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                onClick={handleNext}
+              >
+                Next
+              </button>
             </div>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Stepmessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
